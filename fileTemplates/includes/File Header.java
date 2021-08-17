6b847e423ef1b9e5debe58a317ae407b02225d12
Jaroslav Sendler <jaroslav.sendler@brainpool.cz>
2021-08-17 12:18:36 +0200

    IU-2021.2 <jase@prog10

--- fileTemplates/includes/File Header.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) ${YEAR} by Braingroup AG.
- * All rights reserved.
- *
- * The copyright to the computer program(s) herein is the property of Braingroup AG, Switzerland.
- * The program(s) may be used and/or copied only with the written permission of Braingroup AG or
- * in accordance with the terms and conditions stipulated in the agreement/contract under which
- * the program(s) have been supplied.
- */
